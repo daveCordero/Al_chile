30c5da4dc122ddb38a32de9902742e6e7640b673
Dave Cordero <dvdhec@gmail.com>
2015-05-22 11:36:50 -0600

    v85
    swiper-demos.js editado
    mc-control

--- js/swiper-demos.js
+++ js/swiper-demos.js
@@ -2,6 +2,35 @@
 Author: Vladimir Kharlampidi, The iDangero.us
 */
 $(function(){
+
+	//Movies App
+	var swiperMovies = $('.mc-posters').swiper({
+		mode : "horizontal", 
+		onlyExternal : true,
+		speed:1000
+	});
+	var allowMovieClick = true
+	var swiperMControl = $('.mc-control').swiper({
+		mode : "horizontal", 
+		scrollContainer:true,
+		onTouchMove : function(){
+			allowMovieClick = false	
+		},
+		onTouchEnd : function() {
+			setTimeout(function(){allowMovieClick = true},100)	
+		}
+	});
+	$('.mc-control img').bind('mousedown',function(e){
+		e.preventDefault()
+	})
+	$('.mc-control img').bind('click',function(e){
+		e.preventDefault()
+		if (!allowMovieClick) return;
+		var index = $(this).index()
+		swiperMovies.swipeTo ( index )
+		$('.mc-control .active').removeClass('active')
+		$(this).addClass('active')
+	})
 	
 	/* Dynamic Swiper */
 	function randomColor () {
